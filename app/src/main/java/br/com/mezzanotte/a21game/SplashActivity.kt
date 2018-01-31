829f3437c5ef1a659c53db01303656febe6329a1
logonrm <logonrm@fiap.com.br>
2018-01-31 20:40:55 -0200

    Criação do Projeto

---
+++ app/src/main/java/br/com/mezzanotte/a21game/SplashActivity.kt
@@ -0,0 +1,12 @@
+package br.com.mezzanotte.a21game
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
